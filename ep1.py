6cd93dfe1b526bb3f85604f3557a29982d021172
Juan Ruiz <Juan@Juans-MacBook-Pro.local>
2019-04-16 17:02:52 -0300

    Atualizei os nomes e emials.

--- ep1.py
+++ ep1.py
@@ -1,8 +1,8 @@
 # EP 2019-1: Escape Insper
 #
 # Alunos: 
-# - aluno A: Fulano da Silva, fulanos@insper.edu.br
-# - aluno B: Sicrano de Almeida, sicranoa1@insper.edu.br
+# - aluno A: Juan Greco Ruiz, juangr@al.insper.edu.br
+# - aluno B: Giovanni Augustho Rozatti, giovanniar@al.insper.edu.br
 
 def carregar_cenarios():
     cenarios = {
