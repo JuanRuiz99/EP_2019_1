2d416e5cf9a80b057d73f28e032caf0a0e86c8f2
Giovanni <giovanniar@al.insper.edu.br>
2019-04-22 21:01:18 -0300

    Corrigindo Bugs do sistema de Batalha

--- BattleSystem2.py
+++ BattleSystem2.py
@@ -6,8 +6,18 @@ Created on Sun Apr 21 15:54:18 2019
 """
 from random import randint
 
+
+def carrega_inimigos():
+    inimigos={
+            "professor":"Segundo o programa pode ser um monstro, tome cuidado",
+            "alunos":"Estão tão cansados que nem eles sabem por que eles estão lutando",
+            "você mesmo": "Era só você sair do FabLab e trabalhar no EP, agora você tem que lutar contra si mesmo"
+            }
+    return inimigos
+
 Player_HP=30
-Enemy_HP = 30
+Enemy_HP = 14
+
 print ("Você encontra seu professor")
 print ("Ele te faz uma proposta: se voce derrotar ele a entrega do EP vai ser adiada")
 print ("Se isso não acontecer você morre")
@@ -15,30 +25,33 @@ Batalha = input("Você quer batalhar contra seu professor? s/n: ")
 
 if Batalha != "s" and Batalha !="n":
     print ("Comando Inválido")
-    Batalha = input("Você quer batalhar? s/n")
-while Batalha == "s":
+    Batalha = input("Você quer batalhar? s/n: ")
+while Batalha == "s" and Player_HP>0 and Enemy_HP>0:
     Player_Damage=randint(10,15)
     Enemy_Damage=randint(10,15)
-
-    if Player_HP<0 and Enemy_HP>0:
-        print ("Você perdeu")
-        Batalha = "over"
-        Game_Over=True    
-    elif Player_HP>0 and Enemy_HP<=0:
-        print ("Você venceu")
-        Batalha = "Over"
-    elif Player_HP>0 and Enemy_HP>0:
-            Player_HP -= Enemy_Damage
-            Enemy_HP -= Player_Damage
-            print ("Seu HP é {0} pontos de vida".format(Player_HP))
-            print ("O HP do inimigo é {0} pontos de vida".format(Enemy_HP))
-            Batalha = input("Continuar Batalhando? s/n")
-    elif Player_HP<=0 and Enemy_HP<=0:
-        print("Bem, você morreu. Mas não é legal que você tenha levado alguem pra te fazer companhia?")
-        Game_Over= True
-        Batalha = "over"
-    else:
-        Batalha ="s"
+    Player_HP -= Enemy_Damage
+    Enemy_HP -= Player_Damage
+    print ("Seu HP é {0} pontos de vida".format(Player_HP))
+    print ("O HP do inimigo é {0} pontos de vida".format(Enemy_HP))
+    Batalha = input("Continuar Batalhando? s/n: ")
+    
+if Player_HP>0 and Enemy_HP>0:
+    Player_HP -= Enemy_Damage
+    Enemy_HP -= Player_Damage
+    print ("Seu HP é {0} pontos de vida".format(Player_HP))
+    print ("O HP do inimigo é {0} pontos de vida".format(Enemy_HP))
+    Batalha = input("Continuar Batalhando? s/n: ")
+elif Player_HP<=0 and Enemy_HP>0:
+    print ("Você perdeu")
+    Batalha = "over"
+    Game_Over=True    
+elif Player_HP>0 and Enemy_HP<=0:
+    print ("Você venceu")
+    Batalha = "Over"
+elif Player_HP<=0 and Enemy_HP<=0:
+    print("Bem, você morreu. Mas levou seu inimigo junto")
+    Game_Over= True
+    Batalha = "over"
 
 if Batalha == "Over" :
     print("A batalha acabou, seu professor foi derrotado")
